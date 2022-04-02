c1df7dbe46b13712a4fb2452a019bff1041b1241
coleleg <coleleg@gmail.com>
2022-04-02 18:54:04 -0400

    setup sql schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -2,24 +2,26 @@ DROP TABLE IF EXISTS department;
 DROP TABLE IF EXISTS role;
 DROP TABLE IF EXISTS employee;
 
-CREATE TABLE department {
+CREATE TABLE department (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
-}
+);
 
-CREATE TABLE role {
+CREATE TABLE role (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
+    department_id INTEGER,
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
-}
+);
 
-CREATE TABLE employee {
+CREATE TABLE employee (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    first_name VARHCAR(30) NOT NULL,
-    last_name VARHCAR(30) NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER,
     CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL,
     CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
-}
+);
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,30 @@
-DROP DATABASE IF EXISTS employees;
-CREATE DATABASE employees;
-USE employees;
+INSERT INTO department (name)
+VALUES
+('HR'),
+('Accounting'),
+('Engineering'),
+('Sales');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Head of HR', 50000, 1),
+('HR Specialist', 40000, 1),
+('Senior Accountant', 60000, 2),
+('Assistant Accountant', 45000, 2),
+('Senior Engineer', 100000, 3),
+('Engineer', 80000, 3),
+('Junior Engineer', 60000, 3),
+('Head of Sales', 100000, 4),
+('Sales Rep', 80000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('Jenny', 'Curran', 1, 1),
+('Bubba', 'Blue', 2, 1),
+('Tiger', 'Woods', 3, 3),
+('Michelle', 'Wie', 4, 3),
+('Dan', 'Taylor', 5, 5),
+('Jeremy', 'Clarkson', 6, 5),
+('James', 'May', 7, 5),
+('Richard', 'Hammond', 8, 8),
+('Chris', 'Harris', 9, 8);
